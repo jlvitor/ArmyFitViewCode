187730b3f266eaff781ff5c7055225810b8bbf5e
Jean Lucas Vitor <jeanlucasvitor@gmail.com>
2022-09-28 01:10:26 -0300

    Initial settings

--- ArmyFitViewCode/Screens/LoginViewController.swift
+++ ArmyFitViewCode/Screens/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  ArmyFitViewCode
 //
 //  Created by Jean Lucas Vitor on 28/09/22.
@@ -7,13 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
