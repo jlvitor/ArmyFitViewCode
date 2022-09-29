cff195dbd1f428d76efbdc6b6ef14cf5dc370867
Jean Lucas Vitor <65193042+jlvitor@users.noreply.github.com>
2022-09-28 21:06:41 -0300

    Layout register screen (#2)

--- ArmyFitViewCode/Views/RegisterViewController.swift
+++ ArmyFitViewCode/Views/RegisterViewController.swift
@@ -1,5 +1,5 @@
 //
-//  LoginViewController.swift
+//  RegisterViewController.swift
 //  ArmyFitViewCode
 //
 //  Created by Jean Lucas Vitor on 28/09/22.
@@ -7,8 +7,8 @@
 
 import UIKit
 
-final class LoginViewController: UIViewController {
-    
+class RegisterViewController: UIViewController {
+
     private lazy var logoImage: UIImageView = {
         let image = UIImageView()
         image.image = UIImage(named: "logo")
@@ -27,6 +27,19 @@ final class LoginViewController: UIViewController {
         stack.spacing = 15
         stack.translatesAutoresizingMaskIntoConstraints = false
         return stack
+    }()
+    
+    private lazy var nameTextField: UITextField = {
+        let email = UITextField()
+        email.placeholder = "Nome"
+        email.keyboardType = .alphabet
+        email.autocapitalizationType = .words
+        email.spellCheckingType = .no
+        email.backgroundColor = .white
+        email.font = UIFont.systemFont(ofSize: 17)
+        email.borderStyle = .roundedRect
+        email.translatesAutoresizingMaskIntoConstraints = false
+        return email
     }()
     
     private lazy var emailTextField: UITextField = {
@@ -57,7 +70,21 @@ final class LoginViewController: UIViewController {
         return password
     }()
     
-    private lazy var forgotPasswordStackView: UIStackView = {
+    private lazy var confirmPasswordTextField: UITextField = {
+        let password = UITextField()
+        password.placeholder = "Confirmar senha"
+        password.isSecureTextEntry = true
+        password.keyboardType = .default
+        password.autocapitalizationType = .none
+        password.spellCheckingType = .no
+        password.backgroundColor = .white
+        password.font = UIFont.systemFont(ofSize: 17)
+        password.borderStyle = .roundedRect
+        password.translatesAutoresizingMaskIntoConstraints = false
+        return password
+    }()
+    
+    private lazy var passwordInstructionsStackView: UIStackView = {
         let stack = UIStackView()
         stack.axis = .vertical
         stack.alignment = .trailing
@@ -66,18 +93,18 @@ final class LoginViewController: UIViewController {
         return stack
     }()
     
-    private lazy var forgotPasswordLabel: UILabel = {
+    private lazy var passwordInstructionsLabel: UILabel = {
         let label = UILabel()
-        label.text = "Esqueceu a senha?"
+        label.text = "• A senha deve conter de 8-16 caracteres"
         label.textColor = .white
-        label.font = UIFont.italicSystemFont(ofSize: 12)
+        label.font = UIFont.italicSystemFont(ofSize: 14)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
-    private lazy var loginButton: UIButton = {
+    private lazy var registerButton: UIButton = {
         let btn = UIButton()
-        btn.setTitle("LOGIN", for: .normal)
+        btn.setTitle("CADASTRAR", for: .normal)
         btn.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
         btn.setTitleColor(UIColor(named: "green"), for: .normal)
         btn.backgroundColor = .black
@@ -87,7 +114,7 @@ final class LoginViewController: UIViewController {
         return btn
     }()
     
-    private lazy var newAccountStackView: UIStackView = {
+    private lazy var hasAccountStackView: UIStackView = {
         let stack = UIStackView()
         stack.axis = .horizontal
         stack.alignment = .fill
@@ -97,18 +124,18 @@ final class LoginViewController: UIViewController {
         return stack
     }()
     
-    private lazy var newAccountLabel: UILabel = {
+    private lazy var hasAccountLabel: UILabel = {
         let label = UILabel()
-        label.text = "Ainda não possui conta?"
+        label.text = "Já possui conta?"
         label.textColor = .white
         label.font = UIFont.systemFont(ofSize: 14)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
-    private lazy var newAccountGreenLabel: UILabel = {
+    private lazy var hasAccountGreenLabel: UILabel = {
         let label = UILabel()
-        label.text = "Cadastre-se"
+        label.text = "Fazer login"
         label.textColor = UIColor(named: "green")
         label.font = UIFont.systemFont(ofSize: 14, weight: .semibold)
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -126,18 +153,19 @@ final class LoginViewController: UIViewController {
 }
 
 //MARK: - ViewCode
-extension LoginViewController: ViewCode {
+extension RegisterViewController: ViewCode {
     func buildHierarchy() {
         view.addSubview(logoImage)
         view.addSubview(fieldStackView)
+        fieldStackView.addArrangedSubview(nameTextField)
         fieldStackView.addArrangedSubview(emailTextField)
         fieldStackView.addArrangedSubview(passwordTextField)
-        fieldStackView.addArrangedSubview(forgotPasswordStackView)
-        forgotPasswordStackView.addArrangedSubview(forgotPasswordLabel)
-        view.addSubview(loginButton)
-        view.addSubview(newAccountStackView)
-        newAccountStackView.addArrangedSubview(newAccountLabel)
-        newAccountStackView.addArrangedSubview(newAccountGreenLabel)
+        fieldStackView.addArrangedSubview(confirmPasswordTextField)
+        fieldStackView.addArrangedSubview(passwordInstructionsLabel)
+        view.addSubview(registerButton)
+        view.addSubview(hasAccountStackView)
+        hasAccountStackView.addArrangedSubview(hasAccountLabel)
+        hasAccountStackView.addArrangedSubview(hasAccountGreenLabel)
     }
     
     func setupConstraints() {
@@ -151,16 +179,18 @@ extension LoginViewController: ViewCode {
             fieldStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             fieldStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
 
+            nameTextField.heightAnchor.constraint(equalToConstant: 40),
             emailTextField.heightAnchor.constraint(equalToConstant: 40),
             passwordTextField.heightAnchor.constraint(equalToConstant: 40),
+            confirmPasswordTextField.heightAnchor.constraint(equalToConstant: 40),
             
-            loginButton.topAnchor.constraint(equalTo: fieldStackView.bottomAnchor, constant: 36),
-            loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            loginButton.heightAnchor.constraint(equalToConstant: 40),
-            loginButton.widthAnchor.constraint(equalToConstant: 140),
+            registerButton.topAnchor.constraint(equalTo: fieldStackView.bottomAnchor, constant: 36),
+            registerButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            registerButton.heightAnchor.constraint(equalToConstant: 40),
+            registerButton.widthAnchor.constraint(equalToConstant: 200),
             
-            newAccountStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            newAccountStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            hasAccountStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            hasAccountStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
         ])
     }
     
